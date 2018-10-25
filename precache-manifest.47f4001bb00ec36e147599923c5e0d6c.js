b83347dc55cee0d3e36dd16950dad369cb8de751
Arun Chembilath <arun@acrodelon.com>
2018-10-25 21:09:55 +0530

    Updates

---
+++ precache-manifest.47f4001bb00ec36e147599923c5e0d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e63e49fd19a33a73446",
+    "url": "/gk_assgn_findingfalcone/static/js/runtime~main.6e63e49f.js"
+  },
+  {
+    "revision": "bfe638b7c38f32e4a2f7",
+    "url": "/gk_assgn_findingfalcone/static/js/main.bfe638b7.chunk.js"
+  },
+  {
+    "revision": "2e81e5d49d0f4bca5041",
+    "url": "/gk_assgn_findingfalcone/static/js/1.2e81e5d4.chunk.js"
+  },
+  {
+    "revision": "bfe638b7c38f32e4a2f7",
+    "url": "/gk_assgn_findingfalcone/static/css/main.01300519.chunk.css"
+  },
+  {
+    "revision": "7e25dc0846f78580b1d70a8358a0ef7c",
+    "url": "/gk_assgn_findingfalcone/index.html"
+  }
+];
